fd189b6f3d22cb29f45a6bb545583173a39b79f5
Pablo Araujo <pabloaraujogalo@gmail.com>
2021-02-10 18:26:48 -0300

    EndProgram

---
+++ src/application/Program.java
@@ -0,0 +1,74 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enuns.OrderStatus;
+
+
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter cliente data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.println("Birth Date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.next());
+		
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println("Enter order data: ");
+		System.out.print("Status: ");
+		OrderStatus status = OrderStatus.valueOf(sc.next());
+		
+		Order order = new Order(new Date(), status, client);
+		
+		System.out.print("How many items to this order? ");	
+		int contItems = sc.nextInt();
+		
+		
+		
+		for(int i=0;i<contItems;i++) {
+			System.out.println("Enter  #" + (i+1) + "item data: ");
+			System.out.print("Product Name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.print("Product Price: ");
+			double productPrice = sc.nextDouble();
+			System.out.print("quantity: ");
+			int productQuantity = sc.nextInt();
+			
+			Product product = new Product(productName, productPrice);
+			
+			OrderItem orderItem = new OrderItem(productQuantity, productPrice, product);
+			
+			order.addItems(orderItem);
+			
+		}
+		
+		System.out.println();
+		System.out.println("ORDER SUMMARY:");
+		System.out.println(order);
+		
+		
+		sc.close();
+
+	}
+
+}
